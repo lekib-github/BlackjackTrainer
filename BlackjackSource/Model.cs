a67baf48990c8aa8e4eb8e76a23b3dd3e457c5d2
Branko Lekic <branko.lekic02@gmail.com>
2023-08-23 12:28:35 +0200

    Added usage guidance and improved user experience.

--- BlackjackSource/Model.cs
+++ BlackjackSource/Model.cs
@@ -3,6 +3,7 @@
 using static Console;
 using static BasicStrategy.Action;
 using static Hand.States;
+using static DeckMethods;
 
 internal static class DeckMethods
 {
@@ -12,6 +13,11 @@ internal static class DeckMethods
         (8, 'H'), (8, 'D'), (8, 'S'), (8, 'C'), (9, 'H'), (9, 'D'), (9, 'S'), (9, 'C'), (10, 'H'), (10, 'D'), (10, 'S'), (10, 'C'),
         (11, 'H'), (11, 'D'), (11, 'S'), (11, 'C'), (12, 'H'), (12, 'D'), (12, 'S'), (12, 'C'), (13, 'H'), (13, 'D'), (13, 'S'), (13, 'C'),
         (14, 'H'), (14, 'D'), (14, 'S'), (14, 'C')
+    };
+
+    public static readonly string?[] ValueFace =
+    {
+        null, null, "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
     };
 
     // Fisher-Yates algorithm implementation for Shuffle from: https://stackoverflow.com/questions/108819/best-way-to-randomize-an-array-with-net
@@ -144,17 +150,17 @@ internal class Hand
         switch (State)
         {
             case Pair when Cards[0].Value == 14: // always split aces, not encoded in BasicStrat[]
-                return BasicStrategy.Action.Split;
+                return BasicStrategy.Action.split;
             case Pair:
                 return BasicStrategy.BasicStrat[Total][dealerCardVal].Pair; // no ambiguity for double, pair => exactly 2 cards
-            case Hard when BasicStrategy.BasicStrat[Total][dealerCardVal].Hard == BasicStrategy.Action.Doubl:
-                return CardCount == 2 ? BasicStrategy.Action.Doubl : BasicStrategy.Action.Hit;
-            case Hard when BasicStrategy.BasicStrat[Total][dealerCardVal].Hard == BasicStrategy.Action.Surrender:
-                return CardCount == 2 ? BasicStrategy.Action.Surrender : BasicStrategy.Action.Hit;
+            case Hard when BasicStrategy.BasicStrat[Total][dealerCardVal].Hard == BasicStrategy.Action.doubl:
+                return CardCount == 2 ? BasicStrategy.Action.doubl : BasicStrategy.Action.hit;
+            case Hard when BasicStrategy.BasicStrat[Total][dealerCardVal].Hard == BasicStrategy.Action.surrender:
+                return CardCount == 2 ? BasicStrategy.Action.surrender : BasicStrategy.Action.hit;
             case Hard:
                 return BasicStrategy.BasicStrat[Total][dealerCardVal].Hard;
-            case Soft when BasicStrategy.BasicStrat[Total][dealerCardVal].Soft == BasicStrategy.Action.Doubl:
-                return CardCount == 2 ? BasicStrategy.Action.Doubl : BasicStrategy.Action.Hit;
+            case Soft when BasicStrategy.BasicStrat[Total][dealerCardVal].Soft == BasicStrategy.Action.doubl:
+                return CardCount == 2 ? BasicStrategy.Action.doubl : BasicStrategy.Action.hit;
             case Soft:
                 return BasicStrategy.BasicStrat[Total][dealerCardVal].Soft;
         }
@@ -211,11 +217,11 @@ internal static class BasicStrategy
     private static Queue<Hand>? Turn;
     public enum Action
     {
-        Hit,
-        Stand,
-        Doubl,
-        Surrender,
-        Split
+        hit,
+        stand,
+        doubl,
+        surrender,
+        split
     }
 
     // Basic strategy chart for 4-8 decks, dealer stand on any 17, double allowed after split,
@@ -233,105 +239,105 @@ internal static class BasicStrategy
         new (Action? Hard, Action? Soft, Action? Pair)[] { },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (null, null, Split), (null, null, Split), (null, null, Split),
-            (null, null, Split), (null, null, Split), (null, null, Split), (null, null, Hit), (null, null, Hit),
-            (null, null, Hit), (null, null, Hit)
+            (null, null, null), (null, null, null), (null, null, split), (null, null, split), (null, null, split),
+            (null, null, split), (null, null, split), (null, null, split), (null, null, hit), (null, null, hit),
+            (null, null, hit), (null, null, hit)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Hit, null, null), (Hit, null, null), (Hit, null, null),
-            (Hit, null, null), (Hit, null, null), (Hit, null, null), (Hit, null, null), (Hit, null, null),
-            (Hit, null, null), (Hit, null, null)
+            (null, null, null), (null, null, null), (hit, null, null), (hit, null, null), (hit, null, null),
+            (hit, null, null), (hit, null, null), (hit, null, null), (hit, null, null), (hit, null, null),
+            (hit, null, null), (hit, null, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Hit, null, Split), (Hit, null, Split), (Hit, null, Split),
-            (Hit, null, Split), (Hit, null, Split), (Hit, null, Split), (Hit, null, Hit), (Hit, null, Hit),
-            (Hit, null, Hit), (Hit, null, Hit)
+            (null, null, null), (null, null, null), (hit, null, split), (hit, null, split), (hit, null, split),
+            (hit, null, split), (hit, null, split), (hit, null, split), (hit, null, hit), (hit, null, hit),
+            (hit, null, hit), (hit, null, hit)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Hit, null, null), (Hit, null, null), (Hit, null, null),
-            (Hit, null, null), (Hit, null, null), (Hit, null, null), (Hit, null, null), (Hit, null, null),
-            (Hit, null, null), (Hit, null, null)
+            (null, null, null), (null, null, null), (hit, null, null), (hit, null, null), (hit, null, null),
+            (hit, null, null), (hit, null, null), (hit, null, null), (hit, null, null), (hit, null, null),
+            (hit, null, null), (hit, null, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Hit, null, Hit), (Hit, null, Hit), (Hit, null, Hit),
-            (Hit, null, Split), (Hit, null, Split), (Hit, null, Hit), (Hit, null, Hit), (Hit, null, Hit),
-            (Hit, null, Hit), (Hit, null, Hit)
+            (null, null, null), (null, null, null), (hit, null, hit), (hit, null, hit), (hit, null, hit),
+            (hit, null, split), (hit, null, split), (hit, null, hit), (hit, null, hit), (hit, null, hit),
+            (hit, null, hit), (hit, null, hit)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Hit, null, null), (Doubl, null, null), (Doubl, null, null),
-            (Doubl, null, null), (Doubl, null, null), (Hit, null, null), (Hit, null, null), (Hit, null, null),
-            (Hit, null, null), (Hit, null, null)
+            (null, null, null), (null, null, null), (hit, null, null), (doubl, null, null), (doubl, null, null),
+            (doubl, null, null), (doubl, null, null), (hit, null, null), (hit, null, null), (hit, null, null),
+            (hit, null, null), (hit, null, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Doubl, null, Doubl), (Doubl, null, Doubl), (Doubl, null, Doubl),
-            (Doubl, null, Doubl), (Doubl, null, Doubl), (Doubl, null, Doubl), (Doubl, null, Doubl),
-            (Doubl, null, Doubl), (Hit, null, Hit), (Hit, null, Hit)
+            (null, null, null), (null, null, null), (doubl, null, doubl), (doubl, null, doubl), (doubl, null, doubl),
+            (doubl, null, doubl), (doubl, null, doubl), (doubl, null, doubl), (doubl, null, doubl),
+            (doubl, null, doubl), (hit, null, hit), (hit, null, hit)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Doubl, null, null), (Doubl, null, null), (Doubl, null, null),
-            (Doubl, null, null), (Doubl, null, null), (Doubl, null, null), (Doubl, null, null), (Doubl, null, null),
-            (Doubl, null, null), (Hit, null, null)
+            (null, null, null), (null, null, null), (doubl, null, null), (doubl, null, null), (doubl, null, null),
+            (doubl, null, null), (doubl, null, null), (doubl, null, null), (doubl, null, null), (doubl, null, null),
+            (doubl, null, null), (hit, null, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Hit, null, Split), (Hit, null, Split), (Stand, null, Split),
-            (Stand, null, Split), (Stand, null, Split), (Hit, null, Hit), (Hit, null, Hit), (Hit, null, Hit),
-            (Hit, null, Hit), (Hit, null, Hit)
+            (null, null, null), (null, null, null), (hit, null, split), (hit, null, split), (stand, null, split),
+            (stand, null, split), (stand, null, split), (hit, null, hit), (hit, null, hit), (hit, null, hit),
+            (hit, null, hit), (hit, null, hit)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Hit, null), (Stand, Hit, null), (Stand, Hit, null),
-            (Stand, Doubl, null), (Stand, Doubl, null), (Hit, Hit, null), (Hit, Hit, null), (Hit, Hit, null),
-            (Hit, Hit, null), (Hit, Hit, null)
+            (null, null, null), (null, null, null), (stand, hit, null), (stand, hit, null), (stand, hit, null),
+            (stand, doubl, null), (stand, doubl, null), (hit, hit, null), (hit, hit, null), (hit, hit, null),
+            (hit, hit, null), (hit, hit, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Hit, Split), (Stand, Hit, Split), (Stand, Hit, Split),
-            (Stand, Doubl, Split), (Stand, Doubl, Split), (Hit, Hit, Split), (Hit, Hit, Hit), (Hit, Hit, Hit),
-            (Hit, Hit, Hit), (Hit, Hit, Hit)
+            (null, null, null), (null, null, null), (stand, hit, split), (stand, hit, split), (stand, hit, split),
+            (stand, doubl, split), (stand, doubl, split), (hit, hit, split), (hit, hit, hit), (hit, hit, hit),
+            (hit, hit, hit), (hit, hit, hit)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Hit, null), (Stand, Hit, null), (Stand, Doubl, null),
-            (Stand, Doubl, null), (Stand, Doubl, null), (Hit, Hit, null), (Hit, Hit, null), (Hit, Hit, null),
-            (Surrender, Hit, null), (Hit, Hit, null)
+            (null, null, null), (null, null, null), (stand, hit, null), (stand, hit, null), (stand, doubl, null),
+            (stand, doubl, null), (stand, doubl, null), (hit, hit, null), (hit, hit, null), (hit, hit, null),
+            (surrender, hit, null), (hit, hit, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Hit, Split), (Stand, Hit, Split), (Stand, Doubl, Split),
-            (Stand, Doubl, Split), (Stand, Doubl, Split), (Hit, Hit, Split), (Hit, Hit, Split), (Surrender, Hit, Split),
-            (Surrender, Hit, Split), (Surrender, Hit, Split)
+            (null, null, null), (null, null, null), (stand, hit, split), (stand, hit, split), (stand, doubl, split),
+            (stand, doubl, split), (stand, doubl, split), (hit, hit, split), (hit, hit, split), (surrender, hit, split),
+            (surrender, hit, split), (surrender, hit, split)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Hit, null), (Stand, Doubl, null), (Stand, Doubl, null),
-            (Stand, Doubl, null), (Stand, Doubl, null), (Stand, Hit, null), (Stand, Hit, null), (Stand, Hit, null),
-            (Stand, Hit, null), (Stand, Hit, null)
+            (null, null, null), (null, null, null), (stand, hit, null), (stand, doubl, null), (stand, doubl, null),
+            (stand, doubl, null), (stand, doubl, null), (stand, hit, null), (stand, hit, null), (stand, hit, null),
+            (stand, hit, null), (stand, hit, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Stand, Split), (Stand, Doubl, Split), (Stand, Doubl, Split),
-            (Stand, Doubl, Split), (Stand, Doubl, Split), (Stand, Stand, Stand), (Stand, Stand, Split),
-            (Stand, Hit, Split), (Stand, Hit, Stand), (Stand, Hit, Stand)
+            (null, null, null), (null, null, null), (stand, stand, split), (stand, doubl, split), (stand, doubl, split),
+            (stand, doubl, split), (stand, doubl, split), (stand, stand, stand), (stand, stand, split),
+            (stand, hit, split), (stand, hit, stand), (stand, hit, stand)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Stand, null), (Stand, Stand, null), (Stand, Stand, null),
-            (Stand, Stand, null), (Stand, Stand, null), (Stand, Stand, null), (Stand, Stand, null),
-            (Stand, Stand, null), (Stand, Stand, null), (Stand, Stand, null)
+            (null, null, null), (null, null, null), (stand, stand, null), (stand, stand, null), (stand, stand, null),
+            (stand, stand, null), (stand, stand, null), (stand, stand, null), (stand, stand, null),
+            (stand, stand, null), (stand, stand, null), (stand, stand, null)
         },
         new (Action? Hard, Action? Soft, Action? Pair)[]
         {
-            (null, null, null), (null, null, null), (Stand, Stand, Stand), (Stand, Stand, Stand), (Stand, Stand, Stand),
-            (Stand, Stand, Stand), (Stand, Stand, Stand), (Stand, Stand, Stand), (Stand, Stand, Stand),
-            (Stand, Stand, Stand), (Stand, Stand, Stand), (Stand, Stand, Stand)
+            (null, null, null), (null, null, null), (stand, stand, stand), (stand, stand, stand), (stand, stand, stand),
+            (stand, stand, stand), (stand, stand, stand), (stand, stand, stand), (stand, stand, stand),
+            (stand, stand, stand), (stand, stand, stand), (stand, stand, stand)
         },
     };
 
@@ -340,19 +346,19 @@ internal static class BasicStrategy
         Clear();
         var acting = Turn!.Peek();
         var dealer = Hands![^1];
-        for (var i = 0; i < Hands.Count / 2 + 1; ++i)
+        for (var i = 0; i < Hands.Count; ++i)
         {
-            Write("    ");
+            Write("        ");
         }
-        Write($"Dealer showing: {dealer.Cards[0].Value} ");
+        Write($"Dealer showing: {ValueFace[dealer.Cards[0].Value]} ");
         if (Turn.Count == 1)
         {
             for (var i = 1; i < dealer.CardCount; ++i)
             {
-                Write($"{dealer.Cards[i].Value} ");
+                Write($"{ValueFace[dealer.Cards[i].Value]} ");
             }
         }
-        WriteLine("\n\nYour Hand cards (Total State : Bet):");
+        WriteLine("\n\nYour hand(s) (Total State : Bet):");
 
         foreach (var hand in Hands.TakeWhile(hand => hand != Hands[^1]))
         {
@@ -363,7 +369,7 @@ internal static class BasicStrategy
 
             foreach (var card in hand.Cards)
             {
-                Write($"{card.Value} ");
+                Write($"{ValueFace[card.Value]} ");
             }
 
             Write($"({hand.Total} {hand.State} : {hand.Bet})");
@@ -383,7 +389,7 @@ internal static class BasicStrategy
         WriteLine($"Average bet: {(float)Session.TotalBet/Session.HandsPlayed}");
         WriteLine($"Win/loss per hand: {net/Session.HandsPlayed}");
 
-        Write("Place new bets? Yes/New (session): ");
+        Write("\nPlace new bets? Yes/New (session): ");
         if (ReadLine()!.ToUpper() != "YES") Main();
     }
 
@@ -391,12 +397,12 @@ internal static class BasicStrategy
     {
         for (var i = 1; i <= Session!.ActiveHands; ++i)
         {
-            WriteLine($"Bankroll: {Session.BankrollCurr}\nBet for hand {i}");
+            WriteLine($"Bankroll: {Session.BankrollCurr}\nEnter bet for hand {i}:");
             var bet = int.Parse(ReadLine()!);
             while (Session.BankrollCurr - bet < 0)
             {
                 Clear();
-                WriteLine($"Bankroll: {Session.BankrollCurr}\nBet for hand {i} greater than available bankroll, try again");
+                WriteLine($"Bankroll: {Session.BankrollCurr}\nBet for hand {i} greater than available bankroll, try again:");
                 bet = int.Parse(ReadLine()!);
             }
             Hands!.Add(new Hand(bet, ref Session));
@@ -443,7 +449,7 @@ internal static class BasicStrategy
     {
         switch (act)
         {
-            case Split:
+            case split:
             {
                 var deq = Turn!.Dequeue();
                 var tempTurn = Turn.ToList();
@@ -458,7 +464,7 @@ internal static class BasicStrategy
 
                 var tempHands = Hands!.ToArray();
                 Hands.Clear();
-                for (var i = 0; i < tempHands.Length - Turn.Count; ++i)
+                for (var i = 0; tempHands[i] != deq; ++i)
                 {
                     Hands.Add(tempHands[i]);
                 }
@@ -469,13 +475,13 @@ internal static class BasicStrategy
 
                 return true;
             }
-            case Hit:
+            case hit:
                 return acting.Hit(ref Shoe!);
-            case Doubl:
+            case doubl:
                 return acting.Doubl(ref Shoe!, ref Session!);
-            case Surrender:
+            case surrender:
                 return acting.Surrender(ref Session!);
-            case Stand:
+            case stand:
                 return true;
             default:
                 return false;
@@ -484,10 +490,10 @@ internal static class BasicStrategy
     private static void Main()
     {
         Clear();
-        WriteLine("Enter starting bankroll and number of hands to play");
+        WriteLine("Enter starting bankroll followed by number of hands to play on separate lines:");
         Session = new Stats(int.Parse(ReadLine()!), int.Parse(ReadLine()!));
         Clear();
-        Shoe = DeckMethods.ConstructShoe(8);
+        Shoe = ConstructShoe(8);
 
         while (Session.BankrollCurr > 0)
         {
@@ -508,8 +514,23 @@ internal static class BasicStrategy
                     Draw();
                     if (acting.BJ) break;
 
-                    Write("Write action: ");
-                    Enum.TryParse(ReadLine()!, out Action act);
+                    Write("Write action (Hit/Stand");
+                    if (acting.CardCount == 2)
+                    {
+                        Write("/Double/Surrender");
+                    }
+
+                    if (acting.Cards[0].Value == acting.Cards[1].Value)
+                    {
+                        Write("/Split");
+                    }
+                    Write("): ");
+                    var tempAct = ReadLine()!.ToLower();
+                    if (tempAct == "double")
+                    {
+                        tempAct = "doubl";
+                    }
+                    Enum.TryParse(tempAct, out Action act);
 
                     var dealerCardVal = dealer.Cards[0].Value == 14 ? 11 :
                         dealer.Cards[0].Value < 10 ? dealer.Cards[0].Value : 10;
